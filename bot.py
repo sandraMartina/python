f9da88ada86ff7c7be00b63b3ff350889fcdabd7
Sandra Teletor <64851516+sandraMartina@users.noreply.github.com>
2023-09-07 16:33:05 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,18 +5,5 @@ import subprocess
 import sys
 import socket 
 
-#ip = raw_input("enter the IP Address: ")
-#ort = input("Enter the Port Number: ")
-
-hostname = "192.168.64.129"
-port     = 5555
-#BUFFER_SIZE = 1024 * 128 # 128KB max size of messages, feel free to increase
-# separator string for sending 2 messages in one go
-#SEPARATOR = "<sep>"
-
-
-def run((hostname, port)):
-  sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  sock.run((hostname, port))
-
-run(hostname. port)
+def run():
+os.system("""osascript -e 'tell application "Terminal" to do script "ls -l"'""")
