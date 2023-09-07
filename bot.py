4371a936958ea1afcbb1c86ad26ac3bccf5a7eed
Sandra Teletor <64851516+sandraMartina@users.noreply.github.com>
2023-09-07 16:36:34 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,12 @@
 #In here i thing we have to put or own code
 #  print("Hello World")
+from openpyn import openpyn
 import os
 import subprocess
 import sys
 import socket 
 
-def run():
-os.system("""osascript -e 'tell application "Terminal" to do script "ls -l"'""")
+
+
+subprocess.call(["sudo openpyn --init"], shell=True)
+os.system("sudo openpyn --init")
